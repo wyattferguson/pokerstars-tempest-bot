88c1fef2b5d0683a964450cab0c9ae68ee5d8649
Wyatt <wyattf@gmail.com>
2023-02-14 18:36:36 -0500

    hand read done, game rough almost done, nash next

--- src/game.py
+++ src/game.py
@@ -1,21 +1,62 @@
-import sys
+"""
+Shortcut Keys:
+CTRL + ALT + F = FOLD
+CTRL + ALT + C = CALL
 
+- Figure out nash stack calculation
+- Add hand wallet
+
+"""
+
+import keyboard
 from pyautogui import hotkey
+import logging
 
 from config import *
-from player import *
+from vision import Vision
 
 
 class Game():
-    def __init__(self, testing: bool = True) -> None:
-        self.testing = testing
+    def __init__(self, delay: int = 1) -> None:
+        self.delay = delay
+        self.players = 0
         self.pot = 0
-        self.wallet = 0
-        self.blind = 0
         self.hand = []
+        self.games = 0
+        self.player_pushed = False
+        self.wallet = 1000
+        self.hand_wallet = 0  # FIGURE THIS OUT
+        self.action = False
+        self.vsn = Vision()
+        self.db = DB()
+        self.logger = logging.getLogger()
+        self.setup_logging()
 
-    def next_hand(self):
-        pass
+    def setup_logging(self):
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s | %(message)s')
+        file_handler = logging.FileHandler('logs.log')
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+    def run(self) -> None:
+        # print("Press 's' to start playing.")
+        # keyboard.wait('s')
+        while True:
+
+            new_hand = self.vsn.cards()
+            if new_hand and new_hand != self.hand and len(new_hand) == 2:
+                print(f"New Hand -> {new_hand}")
+                self.hand = new_hand
+                self.games += 1
+                self.call = self.vsn.read_players()
+                self.pot = self.vsn.read_pot()
+                self.wallet = self.vsn.read_wallet()
+                self.player_pushed, self.players = self.vsn.read_players()
+                print(self)
+
+            time.sleep(self.delay)
 
     def random_delay(self):
         delay = round(random.uniform(DELAY_LOWER, DELAY_UPPER), 1)
@@ -23,25 +64,22 @@ class Game():
         if not self.testing:
             time.sleep(delay)
 
-    def all_in(self):
-        print("Move -> Push")
-        if not self.testing:
-            hotkey('ctrl', 'e')
+    def player_action(self) -> None:
+        stack = 1.0
+        nash_row = self.db.get_nash(stack, self.player_pushed, self.hand)
+        print(nash_row)
+        self.action = "Push"
+        # ADD TAKE ACTION HERE AFTER RADOM DELAY
+        self.random_delay()
 
-    def fold(self):
-        print("Move -> Fold")
-        if not self.testing:
-            hotkey('ctrl', 'f')
+    def __str__(self) -> str:
+        game_state = f"POT: {self.pot} | PLYS: {self.players} | WLT: {self.wallet} | GMS: {self.games} | HND: {self.hand} | CLD: {self.player_pushed} | ACT: {self.action}"
+        self.logger(game_state)
+        return game_state
 
 
 if __name__ == "__main__":
-    '''
-    # py game.py 1000 5
+    delay = 2
 
-    wallet = int(sys.argv[1])
-    blind = int(sys.argv[2])
-
-    '''
-
-    game = Game()
-    game.random_delay()
+    play = Game()
+    play.run(delay)
--- src/vision.py
+++ src/vision.py
@@ -1,15 +1,8 @@
-""""
-- Card suit, determine color scores to use
-- Add red check for your turn on the red buttons in the bottom right
 
-CTRL + ALT + F = FOLD
-CTRL + ALT + C = CALL
-
-"""
 import re
 import cv2
-import keyboard
 import mss
+from typing import Tuple
 import numpy as np
 import pytesseract as ocr
 
@@ -19,19 +12,8 @@ ocr.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.ex
 
 
 class Vision():
-    def __init__(self, delay: int = 1) -> None:
-        self.delay = delay
-        self.players = 0
-        self.pot = 0
-        self.hand_players = 0
-        self.hand = []
-        self.games = 1
-        self.call = False
-        self.wallet = 1000
+    def __init__(self) -> None:
         self.snap = mss.mss()
-
-    def blind(self):
-        pass
 
     def cards(self):
         hand = []
@@ -43,45 +25,36 @@ class Vision():
             card_str = ocr.image_to_string(gray_screen, lang='eng', config=ocr_config)
             card_value = card_str.strip()
             if card_value:
-                # card_suit = self.card_suit(idx)
-                if card_value == 0:
-                    card_value = "T"
-                hand.append(f"{card_value}")
-                # print(f"CARD: {card_value}")
-            else:
-                print(f"NO CARD FOUND")
-            # self.popup_image(gray_screen)
+                card_suit = self.card_suit(idx)
+                if card_suit:
+                    if card_value == 0 or card_value == "10":
+                        card_value = "T"
+                    elif len(card_value) > 1:  # fix reading 8/9 as 18/19
+                        card_value = card_value[1:]
 
-        print(hand)
+                    hand.append(f"{card_value}{card_suit}")
+                else:
+                    return []
 
         return hand
 
     def card_suit(self, card_idx: int) -> str:
-        '''
-        NO HAND / EMPTY SEAT: [ 56.  52.  50. 255.]
-        CARD IN HAND:
-        SPADE:
-        HEARTS:
-        DIAMONDS:
-        CLUBS:
-        '''
+        screen_shot = self.screen_shot(CARD_SUIT_LOCAIONS[card_idx], True)
+        suit = False
+        threshold = 0.8
+        max_suit_score = 0
+        for s in SUITS:
+            suit_img = cv2.imread(f"{DIR_PATH}\\suits\\{s}.png")
+            test_suit = cv2.cvtColor(suit_img, cv2.COLOR_BGR2GRAY)
+            scores = cv2.matchTemplate(screen_shot, test_suit, cv2.TM_CCOEFF_NORMED)
+            _, max_val, _, max_loc = cv2.minMaxLoc(scores)
+            if max_val > threshold and max_val > max_suit_score:
+                max_suit_score = max_val
+                suit = s
 
-        screen_shot = self.screen_shot(CARD_SUIT_LOCAIONS[card_idx])
-        avg_color_per_row = np.average(screen_shot, axis=0)
-        img_avg = np.round_(np.average(avg_color_per_row, axis=0), decimals=0)
-        # @ red_value = img_avg.flat[0]
-        print(img_avg)
-        self.popup_image(screen_shot)
+        return suit
 
-    def clear_board(self):
-        self.hand = []
-        self.pot = 0
-        self.action = False
-        self.hand_players = 0
-        self.games += 1
-
-    def save_image(self, img):
-        # save matrix/array as image file
+    def save_image(self, img) -> None:
         img_name = f"{random.randint(1,99999)}"
         isWritten = cv2.imwrite(f"{DIR_PATH}\\suits\\{img_name}.png", img)
 
@@ -92,51 +65,43 @@ class Vision():
         pot_screen = self.screen_shot(POT_LOCATION)
         pot_str = ocr.image_to_string(pot_screen)
         current_pot = re.sub('[^0-9]', '', pot_str.strip())
-        if self.pot != current_pot:
-            self.pot = current_pot
-            print(f"Pot: {self.pot}")
         return current_pot
 
     def read_wallet(self) -> int:
+        wallet = 0
         ocr_config = "--psm 13 --oem 1 -c tessedit_char_whitelist=0123456789(),"
         screen_shot = self.screen_shot(WALLET_LOCATION)
         wallet_text = ocr.image_to_string(screen_shot, lang='eng', config=ocr_config)
         parsed_wallet = wallet_text[wallet_text.find('(') + 1: wallet_text.find(')')]
         if parsed_wallet:
-            self.wallet = re.sub('[^0-9]', '', parsed_wallet.strip())
-            print("Wallet: ", self.wallet)
-        return self.wallet
+            wallet = re.sub('[^0-9]', '', parsed_wallet.strip())
+        return wallet
 
-    def read_players(self):
+    def read_players(self) -> Tuple[bool, int]:
         player_cnt = 0
-        self.hand_players = 0
+        player_push = False
         ocr_config = "--psm 13 --oem 1"
         for i, p in enumerate(PLAYER_SEATS):
             status_img = self.screen_shot(p)
-            # self.popup_image(status_img)
             status_str = ocr.image_to_string(status_img, lang='eng', config=ocr_config)
             status = ''.join(filter(str.isalpha, status_str)).lower()
-            # print(status)
+
             if status in ['cap', 'allin', 'aln', 'alin', 'anin']:
                 player_cnt += 1
-                self.call = True
+                player_push = True
                 status = "Called"
-                self.hand_players += 1
             elif status == 'seat':
                 status = "Empty Seat"
             else:
                 player_cnt += 1
                 status = "Waiting"
 
-            print(f"Player {i + 1}: {status}")
+        #     print(f"Player {i + 1}: {status}")
 
-        print(f"Total Players: {player_cnt}")
-        print(f"Hands Players: {self.hand_players}")
-        print("\n############################\n")
-        self.players = player_cnt
-
-    def game_state(self):
-        pass
+        # print(f"Total Players: {player_cnt}")
+        # print(f"Hands Players: {self.hand_players}")
+        # print("\n############################\n")
+        return (player_push, player_cnt)
 
     def screen_shot(self, location: dict, gray_convert: bool = False):
         screen = np.array(self.snap.grab(location))
@@ -150,21 +115,6 @@ class Vision():
         cv2.waitKey(1)
         time.sleep(2)
 
-    def __str__(self) -> str:
-        return f"POT: {self.pot} | PLYS: {self.players} | WLT: {self.wallet} | GMS: {self.games} | HND: {self.hand}"
-
 
 if __name__ == "__main__":
-    delay = 1
-    vsn = Vision(delay)
-    # print("Press 's' to start playing.")
-    # keyboard.wait('s')
-
-    while True:
-        # vsn.read_pot()
-        # vsn.read_players()
-        # hand = vsn.cards()
-        vsn.card_suit(0)
-        # print("Hand: ", hand)
-        # vsn.read_wallet()
-        time.sleep(delay)
+    pass
