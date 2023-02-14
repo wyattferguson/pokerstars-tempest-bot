d789587dc94339a0e4263c88b4a493130ad8c968
Wyatt <wyattf@gmail.com>
2023-02-14 00:58:00 -0500

    working on suits

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from database import DB
 import random
 import time
 
+DIR_PATH = 'C:\\Code\\poker\\src'
 
 SUITS = ["s", "c", "h", "d"]
 FACE_CARDS = {1: 'A', 11: 'J', 12: 'Q', 13: 'K', 14: 'A'}
@@ -13,14 +14,6 @@ PRECISION = 3
 DELAY_UPPER = 6.2
 DELAY_LOWER = 0.4
 
-
-SUIT_COLORS = {
-    'c': (51, 153, 51),  # '339933' GREEN
-    'h': (204, 0, 0),  # 'cc0000' RED
-    's': (51, 51, 51),  # '333333' GREY
-    'd': (0, 102, 204),  # '0066cc' BLUE
-}
-
 POT_LOCATION = {
     'left': 580,
     'top': 280,
@@ -28,33 +21,86 @@ POT_LOCATION = {
     'height': 32
 }
 
+WALLET_LOCATION = {
+    'left': 600,
+    'top': 670,
+    'width': 150,
+    'height': 25
+}
+
+CARD_OFFSET = 80
+CARD_SUIT_Y_OFFSET = 35
+CARD_SUIT_X_OFFSET = 5
+CARD_SUIT_SQR = 15
+
+
 CARD1_VALUE = {
-    'left': 550,
-    'top': 560,
-    'width': 23,
-    'height': 30
+    'left': 560,
+    'top': 574,
+    'width': 25,
+    'height': 32
 }
 
 CARD2_VALUE = {
-    'left': 623,
+    'left': CARD1_VALUE['left'] + CARD_OFFSET,
     'top': CARD1_VALUE['top'],
     'width': CARD1_VALUE['width'],
     'height': CARD1_VALUE['height']
 }
 
 CARD1_SUIT = {
-    'left': 555,
-    'top': 595,
-    'width': 8,
-    'height': 8
+    'left': CARD1_VALUE['left'] + CARD_SUIT_X_OFFSET,
+    'top': CARD1_VALUE['top'] + CARD_SUIT_Y_OFFSET,
+    'width': CARD_SUIT_SQR,
+    'height': CARD_SUIT_SQR
 }
 
 CARD2_SUIT = {
-    'left': 626,
+    'left': CARD1_SUIT['left'] + CARD_OFFSET,
     'top': CARD1_SUIT['top'],
-    'width': 12,
-    'height': 12
+    'width': CARD_SUIT_SQR,
+    'height': CARD_SUIT_SQR
 }
 
 CARD_VALUE_LOCAIONS = [CARD1_VALUE, CARD2_VALUE]
-CARD_SUIT_LOCAIONS = [CARD2_SUIT]
+CARD_SUIT_LOCAIONS = [CARD1_SUIT]
+
+SEAT_WIDTH = 150
+SEAT_HEIGHT = 25
+
+P1_SEAT = {
+    'left': 55,
+    'top': 510,
+    'width': SEAT_WIDTH,
+    'height': SEAT_HEIGHT
+}
+
+P2_SEAT = {
+    'left': 100,
+    'top': 240,
+    'width': SEAT_WIDTH,
+    'height': SEAT_HEIGHT
+}
+
+P3_SEAT = {
+    'left': 545,
+    'top': 154,
+    'width': SEAT_WIDTH,
+    'height': SEAT_HEIGHT
+}
+
+P4_SEAT = {
+    'left': 1035,
+    'top': 242,
+    'width': SEAT_WIDTH,
+    'height': SEAT_HEIGHT
+}
+
+P5_SEAT = {
+    'left': 1080,
+    'top': 510,
+    'width': SEAT_WIDTH,
+    'height': SEAT_HEIGHT
+}
+
+PLAYER_SEATS = [P1_SEAT, P2_SEAT, P3_SEAT, P4_SEAT, P5_SEAT]
--- src/vision.py
+++ src/vision.py
@@ -1,30 +1,31 @@
 """"
-- Finish card suit reader
-- Check all 5 other players for a "Cap" or "All-In", to set call variable
+- Card suit, determine color scores to use
 - Add red check for your turn on the red buttons in the bottom right
 - setup poker key short cuts
-- Add read current wallet
 
 """
 import re
-
 import cv2
 import keyboard
 import mss
-import numpy
+import numpy as np
 import pytesseract as ocr
-from PIL import Image, ImageOps
 
 from config import *
 
 ocr.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-IMG_PATH = 'C:\\Code\\poker\\src'
-
 
 class Vision():
     def __init__(self, delay: int = 1) -> None:
         self.delay = delay
+        self.players = 0
+        self.pot = 0
+        self.hand_players = 0
+        self.hand = []
+        self.games = 1
+        self.call = False
+        self.wallet = 1000
         self.snap = mss.mss()
 
     def blind(self):
@@ -33,72 +34,122 @@ class Vision():
     def cards(self):
         hand = []
         ocr_config = "--psm 13 --oem 1 -c tessedit_char_whitelist=A2345678910JQK"
-        # image_file = 'C:\\Code\\poker\\src\\two.png'
-        # img = Image.open(image_file)
-        # grey_scale = ImageOps.grayscale(img)
 
-        # self.popup_image(numpy.array(grey_scale))
+        for idx, c in enumerate(CARD_VALUE_LOCAIONS):
+            gray_screen = self.screen_shot(c, True)
 
-        # for idx, c in enumerate(CARD_VALUE_LOCAIONS):
-        #     screen_shot = self.screen_shot(c)
-        #     gray_screen = cv2.cvtColor(screen_shot, cv2.COLOR_BGR2GRAY)
+            card_str = ocr.image_to_string(gray_screen, lang='eng', config=ocr_config)
+            card_value = card_str.strip()
+            if card_value:
+                # card_suit = self.card_suit(idx)
+                if card_value == 0:
+                    card_value = "T"
+                hand.append(f"{card_value}")
+                # print(f"CARD: {card_value}")
+            else:
+                print(f"NO CARD FOUND")
+            # self.popup_image(gray_screen)
 
-        #     screen_str = ocr.image_to_string(gray_screen, lang='eng', config=ocr_config)
-        #     card_value = screen_str.strip()
-        #     if card_value:
-        #         card_suit = self.card_suit(idx)
-        #         if card_value == 0:
-        #             card_value = "T"
-        #         hand.append(f"{card_value}{card_suit}")
-        #         print(f"CARD: {card_value}")
-        #     else:
-        #         print(f"NO CARD FOUND")
-        #     # self.popup_image(gray_screen)
-
-        for s in CARD_SUIT_LOCAIONS:
-            screen_shot = self.screen_shot(s)
-            self.popup_image(screen_shot)
+        print(hand)
 
         return hand
+
+    def card_suit(self, card_idx: int) -> str:
+        '''
+        NO HAND / EMPTY SEAT: [ 56.  52.  50. 255.]
+        CARD IN HAND:
+        SPADE:
+        HEARTS:
+        DIAMONDS:
+        CLUBS:
+        '''
+
+        screen_shot = self.screen_shot(CARD_SUIT_LOCAIONS[card_idx])
+        avg_color_per_row = np.average(screen_shot, axis=0)
+        img_avg = np.round_(np.average(avg_color_per_row, axis=0), decimals=0)
+        # @ red_value = img_avg.flat[0]
+        print(img_avg)
+        self.popup_image(screen_shot)
+
+    def clear_board(self):
+        self.hand = []
+        self.pot = 0
+        self.action = False
+        self.hand_players = 0
+        self.games += 1
 
     def save_image(self, img):
         # save matrix/array as image file
         img_name = f"{random.randint(1,99999)}"
-        isWritten = cv2.imwrite(f"{IMG_PATH}\\suits\\{img_name}.png", img)
+        isWritten = cv2.imwrite(f"{DIR_PATH}\\suits\\{img_name}.png", img)
 
         if isWritten:
             print('Image is successfully saved as file.')
 
-    def card_suit(self, card_idx: int) -> str:
-        screen_shot = self.screen_shot(CARD_SUIT_LOCAIONS[card_idx])
-
-        card_suit_part = "c"
-        card_img = cv2.imread(
-            f"{IMG_PATH}\\suits\\{card_suit_part}{card_idx}.png")
-        card_sect = numpy.array(card_img)
-        scr_remove = card_sect[:, :, :3]
-
-        result = cv2.matchTemplate(
-            scr_remove, screen_shot, cv2.TM_CCOEFF_NORMED)
-        print(result)
-
-    def pot(self) -> int:
+    def read_pot(self) -> int:
         pot_screen = self.screen_shot(POT_LOCATION)
         pot_str = ocr.image_to_string(pot_screen)
         current_pot = re.sub('[^0-9]', '', pot_str.strip())
-        print(f"Pot: {current_pot}")
+        if self.pot != current_pot:
+            self.pot = current_pot
+            print(f"Pot: {self.pot}")
         return current_pot
+
+    def read_wallet(self) -> int:
+        ocr_config = "--psm 13 --oem 1 -c tessedit_char_whitelist=0123456789(),"
+        screen_shot = self.screen_shot(WALLET_LOCATION)
+        wallet_text = ocr.image_to_string(screen_shot, lang='eng', config=ocr_config)
+        parsed_wallet = wallet_text[wallet_text.find('(') + 1: wallet_text.find(')')]
+        if parsed_wallet:
+            self.wallet = re.sub('[^0-9]', '', parsed_wallet.strip())
+            print("Wallet: ", self.wallet)
+        return self.wallet
+
+    def read_players(self):
+        player_cnt = 0
+        self.hand_players = 0
+        ocr_config = "--psm 13 --oem 1"
+        for i, p in enumerate(PLAYER_SEATS):
+            status_img = self.screen_shot(p)
+            # self.popup_image(status_img)
+            status_str = ocr.image_to_string(status_img, lang='eng', config=ocr_config)
+            status = ''.join(filter(str.isalpha, status_str)).lower()
+            # print(status)
+            if status in ['cap', 'allin', 'aln', 'alin', 'anin']:
+                player_cnt += 1
+                self.call = True
+                status = "Called"
+                self.hand_players += 1
+            elif status == 'seat':
+                status = "Empty Seat"
+            else:
+                player_cnt += 1
+                status = "Waiting"
+
+            print(f"Player {i + 1}: {status}")
+
+        print(f"Total Players: {player_cnt}")
+        print(f"Hands Players: {self.hand_players}")
+        print("\n############################\n")
+        self.players = player_cnt
 
     def game_state(self):
         pass
 
-    def screen_shot(self, location):
-        return numpy.array(self.snap.grab(location))
+    def screen_shot(self, location: dict, gray_convert: bool = False):
+        screen = np.array(self.snap.grab(location))
+        if gray_convert:
+            gray_screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+            return gray_screen
+        return screen
 
     def popup_image(self, image):
         cv2.imshow('Screen Shot', image)
         cv2.waitKey(1)
-        time.sleep(5)
+        time.sleep(2)
+
+    def __str__(self) -> str:
+        return f"POT: {self.pot} | PLYS: {self.players} | WLT: {self.wallet} | GMS: {self.games} | HND: {self.hand}"
 
 
 if __name__ == "__main__":
@@ -108,7 +159,10 @@ if __name__ == "__main__":
     # keyboard.wait('s')
 
     while True:
-        # vsn.pot()
-        hand = vsn.cards()
-        print("Hand: ", hand)
+        # vsn.read_pot()
+        # vsn.read_players()
+        # hand = vsn.cards()
+        vsn.card_suit(0)
+        # print("Hand: ", hand)
+        # vsn.read_wallet()
         time.sleep(delay)
